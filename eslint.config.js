fa079dd738212b65c3d578946561bbc42ccaa6a7
Isaac <isaacherrera47@gmail.com>
2024-10-29 12:18:55 -0600

    fix config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    extends: [
-      'eslint:recommended',
-    ],
+    files: ["**/*.js"],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     rules: {
+      ...require('eslint-config-eslint/recommended').rules,
       'no-console': 'warn', // Warn on console.log statements
       'semi': ['error', 'always'], // Require semicolons
       'quotes': ['error', 'single'], // Require single quotes
